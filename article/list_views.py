1d8e02997f90d4f0f4b46590a398d9f12083a210
Your Name <email@example.com>
2017-12-15 20:17:31 +0800

    lookup

--- article/list_views.py
+++ article/list_views.py
@@ -17,7 +17,7 @@ from django.conf import settings
 r = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
 
 
-@login_required(login_url='/account/login/')
+@login_required(login_url='/account/new_login/')
 @require_POST
 @csrf_exempt
 def like_article(request):
